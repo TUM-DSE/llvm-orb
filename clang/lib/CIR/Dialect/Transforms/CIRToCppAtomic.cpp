c2700c2d690bf23bcbc82c07bf96a635f21a0c05
Anaklysmos28 <alex_bartelt@gmx.de>
2026-06-19 10:44:25 +0000

    added conversion patterns for load store

---
+++ clang/lib/CIR/Dialect/Transforms/CIRToCppAtomic.cpp
@@ -0,0 +1,124 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "PassDetail.h"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/Orb/CppAtomicDialect.h"
+#include "mlir/IR/Block.h"
+#include "mlir/IR/Operation.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/IR/Region.h"
+#include "mlir/Support/LogicalResult.h"
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
+#include "mlir/Transforms/WalkPatternRewriteDriver.h"
+#include "mlir/Transforms/DialectConversion.h"
+#include "clang/CIR/Dialect/IR/CIRDialect.h"
+#include "clang/CIR/Dialect/Passes.h"
+#include "llvm/ADT/SmallVector.h"
+
+using namespace mlir;
+using namespace cir;
+
+namespace mlir {
+#define GEN_PASS_DEF_CIRTOCPPATOMIC
+#include "clang/CIR/Dialect/Passes.h.inc"
+} // namespace mlir
+
+//===----------------------------------------------------------------------===//
+// Rewrite patterns
+//===----------------------------------------------------------------------===//
+
+namespace {
+
+static cpp_atomic::MemoryOrder convertMemoryOrder(std::optional<cir::MemOrder> cirOrder) {
+
+  if (!cirOrder.has_value()) {
+    return cpp_atomic::MemoryOrder::SeqCst;
+  }
+
+  switch (cirOrder) {
+    case cir::MemOrder::Relaxed: return cpp_atomic::MemoryOrder::Relaxed;
+    case cir::MemOrder::Acquire: return cpp_atomic::MemoryOrder::Acquire;
+    case cir::MemOrder::Release: return cpp_atomic::MemoryOrder::Release;
+    case cir::MemOrder::AcquireRelease: return cpp_atomic::MemoryOrder::AcqRel;
+    default: 
+      return cpp_atomic::MemoryOrder::SeqCst;
+  }
+}  
+
+struct LoadRewriter : public OpConversionPattern<cir::LoadOp> {
+
+  LoadRewriter(MLIRContext *context)
+      : OpConversionPattern<cir::LoadOp>(context) {}
+
+  LogicalResult matchAndRewrite(cir::LoadOp loadOp, OpAdaptor adaptor,
+                                ConversionPatternRewriter &rewriter) const override {
+
+    Value addr = loadOp.getAddr();
+
+    auto memOrder = convertMemoryOrder(loadOp.getMemOrder());
+    auto alignment = loadOp.getAlignmentAttr();
+    
+    rewriter.replaceOpWithNewOp<cpp_atomic::AtomicLoadOp>(loadOp, addr, memOrder, alignment);
+    return success();
+  }
+};
+
+struct StoreRewriter : public OpConversionPattern<cir::StoreOp> {
+
+  StoreRewriter(MLIRContext *context)
+      : OpConversionPattern<cir::StoreOp>(context) {}
+
+  LogicalResult matchAndRewrite(cir::StoreOp storeOp, OpAdaptor adaptor,
+                                ConversionPatternRewriter &rewriter) const override {
+
+    Value value = storeOp.getValue();                             
+    Value addr = storeOp.getAddr();
+
+    auto memOrder = convertMemoryOrder(loadOp.getMemOrder());
+    auto alignment = loadOp.getAlignmentAttr();
+    
+    rewriter.replaceOpWithNewOp<cpp_atomic::AtomicStoreOp>(storeOp, value, addr, memOrder, alignment);
+    return success();
+  }
+};
+
+void populateCIRToCppAtomicPatterns(RewritePatternSet &patterns) {
+  patterns.add<LoadRewriter>(patterns.getContext());
+  patterns.add<StoreRewriter>(patterns.getContext());
+}
+
+//===----------------------------------------------------------------------===//
+// Pass definition
+// ===----------------------------------------------------------------------===//
+
+struct CIRToCppAtomicPass : public impl::CIRToCppAtomicBase<CIRToCppAtomicPass> {
+  CIRToCppAtomicPass() = default;
+  void runOnOperation() override;
+};
+
+void CIRToCppAtomicPass::runOnOperation() {
+  MLIRContext *context = &getContext();
+  ConversionTarget target(*context);
+  target.addLegalDialect<cpp_atomic::CppAtomicDialect>();
+  target.addLegalOp<mlir::UnrealizedConversionCastOp>();
+  target.addIllegalOp<cir::LoadOp, cir::StoreOp>();
+
+  RewritePatternSet patterns(context);
+
+  populateCIRToCppAtomicPatterns(patterns);
+
+  if (failed(applyPartialConversion(getOperation(), target, std::move(patterns))))
+    signalPassFailure();
+}
+
+} // namespace
+
+std::unique_ptr<Pass> mlir::createCIRToCppAtomicPass() {
+  return std::make_unique<CIRToCppAtomicPass>();
+}
