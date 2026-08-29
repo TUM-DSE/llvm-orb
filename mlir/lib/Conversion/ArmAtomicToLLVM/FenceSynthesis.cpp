380ef20adee5b48519153c360352c6d396e60b29
ReimersS <sebastian.reimers@tum.de>
2026-08-29 15:01:02 +0000

    [FenceSynthesis] more closure rounds

--- mlir/lib/Conversion/ArmAtomicToLLVM/FenceSynthesis.cpp
+++ mlir/lib/Conversion/ArmAtomicToLLVM/FenceSynthesis.cpp
@@ -474,7 +474,7 @@ struct FenceSynthesisPass
         unsigned fOrigIdx = mb.idxOf(newId);
         mb.addIntermediateFence(fOrigIdx, iface, dom, postDom, reach);
         mb.markOrdered(bestIdA, bestIdB);
-        mb.closeTransitively(iface, 6);
+        mb.closeTransitively(iface);
         ++iteration;
         {
           auto [c, o] = mb.orderedCounts();
@@ -485,7 +485,7 @@ struct FenceSynthesisPass
       }
       iface->updateOrderMatrix(bestPromotion, newOp, bestIdA, bestIdB,
                                mb, aa, dom, postDom, reach);
-      mb.closeTransitively(iface, 6);
+      mb.closeTransitively(iface);
       ++iteration;
       {
         auto [c, o] = mb.orderedCounts();
